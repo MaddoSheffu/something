40e11881bff2edb638e79af144022c7b2341ea94
MaddoSheffu <53875744+MaddoSheffu@users.noreply.github.com>
2020-04-19 17:19:48 +0200

    path movement

---
+++ Antoria/Assets/Scripts/NPC_AI.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NPC_AI : MonoBehaviour
+{
+    public float agentSpeed;
+    public Transform waypoint;
+    public NavMeshAgent agent;
+    void Start()
+    {
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        if (agent != null)
+        { 
+            Walk(); 
+        }
+    }
+    void Walk()
+    {
+
+        agent.destination = waypoint.position;
+    }
+
+}
